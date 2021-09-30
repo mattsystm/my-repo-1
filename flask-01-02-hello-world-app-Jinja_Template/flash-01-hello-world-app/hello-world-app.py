f58e8214fea6c8614d6f639ca13843ba4f40fdec
mattsystm <mattsystm@gmail.com>
2021-09-29 21:36:07 -0400

    My first app with Flask

---
+++ flask-01-02-hello-world-app-Jinja_Template/flash-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return "Hello World!"
+
+@app.route("/second")
+def second():
+    return "This is the second page"
+
+if __name__=="__main__":
+    app.run(debug=True)
